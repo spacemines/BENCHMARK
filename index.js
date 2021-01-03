3553728297d4359f0ed3064615cadb4e9b9c8dd3
fish <t.nych.w@gmail.com>
2021-01-02 23:56:16 -0500

    complete add items

--- index.js
+++ index.js
@@ -22,15 +22,21 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 
 db.once('open', function () {
-    // we're connected!
-    console.log('connect successfully')
+    console.log('connect to db successfully')
+})
+
+const itemSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
+    quantity: { type: Number }
 })
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    items: { type: Map, of: Number, default: new Map() }
 })
+
+const itemModel = mongoose.model('Item', itemSchema)
 
 const userModel = mongoose.model('User', userSchema)
 
@@ -51,9 +57,8 @@ app.get('/users/login', function (req, res) {
 app.post('/users/login', async function (req, res) {
     // verify username and password 
     const { username, password } = req.body
-    console.log(username)
-    const isUserExists = await userModel.countDocuments({ username, password })
-    if (isUserExists <= 0) {
+    const user= await userModel.findOne({ username, password })
+    if (!user) {
         res.status(400).json({ 
             message: 'incorrect username or password'
         })
@@ -78,8 +83,8 @@ app.get('/users/create', function (req, res) {
 app.post('/users/create', async function (req, res) {
     // verify username is valid
     const { username, password } = req.body
-    const isUserExists = await userModel.countDocuments({ username })
-    if (isUserExists > 0) {
+    const user = await userModel.findOne({ username })
+    if (user) {
         res.status(400).json({ 
             message: 'username already exists'
         })
@@ -103,12 +108,9 @@ app.post('/users/create', async function (req, res) {
 app.get('/items/create', function (req, res) {
     res.sendFile(__dirname + '/addItem.html')
 })
-app.post('/items/create', function(req, res) {
+app.post('/items/create', async function(req, res) {
     // verify token
-    const { token, item, quantity } = req.body
-    console.log(token)
-    console.log(item)
-    console.log(quantity)
+    const { token, name, quantity } = req.body
     if (!token) {
         res.status(400).json({
             message: 'authentication token required'
@@ -119,33 +121,32 @@ app.post('/items/create', function(req, res) {
     var payload
     try {
         payload = jwt.verify(token, jwtKey)
+        username = payload.username
     } catch (err) {
         res.status(400).end()
     }
     
     // check token expiration
-    if (Date.now() > payload.exp) {
+    // Date.now() returns unix timestamp in milliseconds
+    if (Date.now()/1000 > payload.exp) {
         res.status(400).json({ 
             message: 'authentication token expired'
         })
     }
 
-    // get users cart
-    const user = userModel.find({ username: payload.username })
-    const cart = user.cart
-    if (cart.has(item)) {
-        cart[item] += quantity
+    // create or update item
+    const item = await itemModel.findOne({ username, name })
+    if (item) {
+        item.quantity += parseInt(quantity)
+        await item.save()
     } else {
-        cart[item] = quantity
+        await itemModel.create({ username, name, quantity })
     }
-    
+
     // send item
+    const itm = await itemModel.findOne({username, name})
     res.status(200).json({
-        item: {
-            username: payload.username,
-            name: item,
-            quantity: quantity
-        }
+        item: itm
     })
 })
 
@@ -163,29 +164,60 @@ app.get('/items/get/:token', async function (req, res) {
     var payload
     try {
         payload = jwt.verify(token, jwtKey)
+        username = payload.username
     } catch (err) {
         res.status(400).end()
     }
     
     // check token expiration
-    if (Date.now() > payload.exp) {
+    if (Date.now()/1000 > payload.exp) {
         res.status(400).json({ 
             message: 'authentication token expired'
         })
     }
 
     // find user
-    const user = userModel.find({ username: payload.username })
+    const user = userModel.findOne({ username })
     if (!user) {
         res.status(400).json({
             message: 'couldn\'t find user'
         })
     }
 
+    const items = await itemModel.find({ username })
+    console.log(items)
     // render their cart
-    res.render('cart.ejs', { cart: payload.cart })
+    res.render('cart.ejs', { items: items })
 })
-app.post('items/remove', function (req, res) {
+app.post('items/remove', async function (req, res) {
+    // verify token
+    const { token, _id } = req.body
+    if (!token) {
+        res.status(400).json({
+            message: 'authentication token required'
+        })
+    }
 
+    // get payload
+    var payload
+    try {
+        payload = jwt.verify(token, jwtKey)
+        username = payload.username
+    } catch (err) {
+        res.status(400).end()
+    }
+
+    // check token expiration
+    if (Date.now()/1000 > payload.exp) {
+        res.status(400).json({
+            message: 'authentication token expired'
+        })
+    }
+    
+    const item = await itemModel.findOne({ _id: _id, username: username })
+    await itemModel.delete(item)
+    res.status(200).json({
+        item
+    }) 
 })
 
